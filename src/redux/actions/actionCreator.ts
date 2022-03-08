8b1dded258fdc9a5f77c92a72fed6868edfa504a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-08 13:00:31 +0100

    added loadSneakerInfo action to get the data of one sneaker

--- src/redux/actions/actionCreator.ts
+++ src/redux/actions/actionCreator.ts
@@ -5,7 +5,16 @@ import actionTypes from "./actionTypes";
 interface loadSneakers extends Action {
   sneakers: Sneaker[];
 }
+interface loadSneakerInfo extends Action {
+  sneaker: Sneaker;
+}
+
 export const loadSneakersAction = (sneakers: Sneaker[]): loadSneakers => ({
   type: actionTypes.loadSneakers,
-  sneakers: sneakers,
+  sneakers,
+});
+
+export const loadSneakerInfoAction = (sneaker: Sneaker): loadSneakerInfo => ({
+  type: actionTypes.loadSneakerInfo,
+  sneaker,
 });
