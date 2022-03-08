3ee43ccc5aea80841f4f2b28babcc9200f868b15
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-08 14:28:49 +0100

    added a register action with interface

--- src/redux/actions/usersActionCreator/usersActionCreator.ts
+++ src/redux/actions/usersActionCreator/usersActionCreator.ts
@@ -2,10 +2,15 @@ import { Action } from "../../../Types/Action";
 import { User } from "../../../Types/User";
 import actionTypes from "../actionTypes";
 
-interface LoginAction extends Action {
+interface RegisterAndLoginAction extends Action {
   user: User;
 }
-export const loginAction = (user: User): LoginAction => ({
+export const loginAction = (user: User): RegisterAndLoginAction => ({
   type: actionTypes.login,
+  user,
+});
+
+export const registerAction = (user: User): RegisterAndLoginAction => ({
+  type: actionTypes.register,
   user,
 });
