9a54a58b2fc1b6225acde42456458aed50453913
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-09 19:42:23 +0100

    added sneakerList component with prop and types

---
+++ src/components/SneakersList/SneakersList.tsx
@@ -0,0 +1,9 @@
+import { Sneaker } from "../../Types/Sneaker";
+
+interface SneakersListProps {
+  sneakers: Sneaker[];
+}
+const SneakersList = ({ sneakers }: SneakersListProps): JSX.Element => {
+  return <></>;
+};
+export default SneakersList;
