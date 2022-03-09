b2894bfd03d656ed0156d745e7ef80eb808defcc
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-09 17:49:50 +0100

    reducer returned undefined if the payload was missing, fixed

--- src/redux/reducers/sneakers/sneakersReducer.ts
+++ src/redux/reducers/sneakers/sneakersReducer.ts
@@ -10,17 +10,21 @@ const sneakersReducer = (
   currentState: Sneaker[] | [] = [],
   action: Action = {}
 ) => {
-  let newSneakers;
-
+  let newSneakers: Sneaker[] | Sneaker;
   switch (action.type) {
     case actionTypes.loadSneakers:
       if ((action as LoadSneakersAction).sneakers) {
-        return (newSneakers = [...(action as LoadSneakersAction).sneakers]);
+        newSneakers = [...(action as LoadSneakersAction).sneakers];
+      } else {
+        newSneakers = [...currentState];
       }
       break;
+
     case actionTypes.loadSneakerInfo:
       if ((action as LoadSneakerInfoAction).sneaker) {
-        return (newSneakers = { ...(action as LoadSneakerInfoAction).sneaker });
+        newSneakers = { ...(action as LoadSneakerInfoAction).sneaker };
+      } else {
+        newSneakers = [...currentState];
       }
       break;
     default:
