ea16d86f4f492c629b625a5bdaa426dcb64ff28a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-10 12:58:13 +0100

    added styles to the container of the whole results

---
+++ src/pages/SneakersResultsPage/SneakerResultsList.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SneakersResultsListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+
+  & .sneakerResults_tittle {
+    margin-bottom: 0px;
+  }
+`;
--- src/pages/SneakersResultsPage/SneakersResultsPage.tsx
+++ src/pages/SneakersResultsPage/SneakersResultsPage.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import SneakersList from "../../components/SneakersList/SneakersList";
 import { loadAllSneakersThunk } from "../../redux/thunks/sneakersThunk";
 import { Sneaker } from "../../Types/Sneaker";
+import { SneakersResultsListContainer } from "./SneakerResultsList";
 
 const SneakerResultsPage = (): JSX.Element => {
   const sneakers: Sneaker[] = useSelector((state: RootState) => state.sneakers);
@@ -15,8 +16,10 @@ const SneakerResultsPage = (): JSX.Element => {
 
   return (
     <>
-      <h1>RESULTS:</h1>
-      <SneakersList sneakers={sneakers} />
+      <SneakersResultsListContainer>
+        <h1 className="sneakerResults_tittle">RESULTS:</h1>
+        <SneakersList sneakers={sneakers} />
+      </SneakersResultsListContainer>
     </>
   );
 };
