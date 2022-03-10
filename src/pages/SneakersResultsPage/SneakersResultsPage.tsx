2f1da1812cef5d4a38a7f89cd6b6a26e5dbee874
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-10 11:18:34 +0100

    added useeffect in sneakers page and provider in index

--- src/pages/SneakersResultsPage/SneakersResultsPage.tsx
+++ src/pages/SneakersResultsPage/SneakersResultsPage.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { RootState } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import SneakersList from "../../components/SneakersList/SneakersList";
+import { loadAllSneakersThunk } from "../../redux/thunks/sneakersThunk";
+
 const SneakerResultsPage = (): JSX.Element => {
+  const sneakers = useSelector((state: RootState) => state.sneakers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadAllSneakersThunk);
+  });
+
   return (
     <>
       <h1>RESULTS:</h1>
+      <SneakersList sneakers={sneakers} />
     </>
   );
 };
