5f34d69c59991039b7099645a1d40180fd9a74c6
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-10 20:25:35 +0100

    added useEffect to load data on sneaker info

--- src/pages/SneakerInfoPage/SneakerInfoPage.tsx
+++ src/pages/SneakerInfoPage/SneakerInfoPage.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { RootState } from "../../redux/store";
+import { moreInfoSneakerThunk } from "../../redux/thunks/sneakersThunk";
+import { Sneaker } from "../../Types/Sneaker";
 
 const SneakerInfoPage = (): JSX.Element => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(moreInfoSneakerThunk(id as string));
+  }, [dispatch, id]);
 
   return <></>;
 };
