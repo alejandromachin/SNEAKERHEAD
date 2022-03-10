45dc8d5853cf269aaa0b6dd549815c704d361b4f
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-10 20:51:04 +0100

    added useEffect to change the state of sneaker data

--- src/pages/SneakerInfoPage/SneakerInfoPage.tsx
+++ src/pages/SneakerInfoPage/SneakerInfoPage.tsx
@@ -8,11 +8,25 @@ import { Sneaker } from "../../Types/Sneaker";
 const SneakerInfoPage = (): JSX.Element => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const sneaker = useSelector((state: RootState) => state.sneaker);
 
   useEffect(() => {
     dispatch(moreInfoSneakerThunk(id as string));
   }, [dispatch, id]);
 
-  return <></>;
+  return (
+    <>
+      <img
+        src={(sneaker as Sneaker).image}
+        alt={(sneaker as Sneaker).colorway}
+      />
+      <p className="sneaker_name">
+        {(sneaker as Sneaker).brand} {(sneaker as Sneaker).style}
+      </p>
+      <p>
+        {(sneaker as Sneaker).colorway} | {(sneaker as Sneaker).averagePrice}
+      </p>
+    </>
+  );
 };
 export default SneakerInfoPage;
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import adsReducer from "./adsReducer/adsReducer";
+import sneakerReducer from "./sneaker/sneakerReducer";
 import sneakersReducer from "./sneakers/sneakersReducer";
 
 const rootReducer = combineReducers({
   sneakers: sneakersReducer,
+  sneaker: sneakerReducer,
   ads: adsReducer,
 });
 
