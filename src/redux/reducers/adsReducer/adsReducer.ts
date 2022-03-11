71d0f89d2c5e3f3dca98d1ddd29199defe53eb96
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-11 17:32:41 +0100

    fixed bug in adds reducer

--- src/redux/reducers/adsReducer/adsReducer.ts
+++ src/redux/reducers/adsReducer/adsReducer.ts
@@ -1,10 +1,18 @@
-import { Action, AdAction, DeleteAdAction } from "../../../Types/Action";
+import {
+  Action,
+  AdAction,
+  DeleteAdAction,
+  LoadAdsAction,
+} from "../../../Types/Action";
 import { Ad } from "../../../Types/Ad";
 import actionTypes from "../../actions/actionTypes";
 
 const adsReducer = (currentState: Ad[] = [], action: Action = {}): Ad[] => {
   let newAds;
   switch (action.type) {
+    case actionTypes.loadAds:
+      newAds = [...(action as LoadAdsAction).ads];
+      break;
     case actionTypes.deleteAd:
       newAds = currentState.filter(
         (ad: Ad) => ad.id !== (action as DeleteAdAction).id
