0c5dc52b4403954852d132e45ca35a714c4dedb3
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-11 15:28:35 +0100

    added sneakerAd component basic function

---
+++ src/components/SneakerAd/SneakerAd.tsx
@@ -0,0 +1,10 @@
+import { Ad } from "../../Types/Ad";
+
+interface SneakerAdProps {
+  ad: Ad;
+}
+
+const SneakerAd = ({ ad }: SneakerAdProps): JSX.Element => {
+  return <></>;
+};
+export default SneakerAd;
