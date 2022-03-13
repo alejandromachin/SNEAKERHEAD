6cbc39a831ff788ecf6cc28bb6ea6a2760df2d59
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-13 19:34:02 +0100

    added useeffect to dispatch the thunk action

--- src/pages/AdinfoPage/AdinfoPage.tsx
+++ src/pages/AdinfoPage/AdinfoPage.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { RootState } from "../../redux/store";
+import { moreInfoAdThunk } from "../../redux/thunks/adsThunks";
 
 const AdinfoPage = (): JSX.Element => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const ad = useSelector((state: RootState) => state.ad);
 
+  useEffect(() => {
+    dispatch(moreInfoAdThunk(id as string));
+  });
   return <></>;
 };
 export default AdinfoPage;
