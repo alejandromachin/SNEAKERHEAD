a83d0c3bd5ad17c268af584e5b0459074ce8ef09
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-13 20:59:30 +0100

    changed names page to AdMoreInfoPage

--- src/pages/AdMoreInfoPage/AdMoreInfoPage.test.tsx
+++ src/pages/AdMoreInfoPage/AdMoreInfoPage.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
-import AdInfoPage from "./AdInfoPage";
+
+import AdInfoPage from "./AdMoreInfoPage";
 
 const mockUseParams = { id: "test" };
 
