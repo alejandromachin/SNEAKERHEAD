336b1b601f0c9585392eae4ffebdb451acfd7d77
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-14 22:51:57 +0100

    added test on onclick action

--- src/pages/AdMoreInfoPage/AdMoreInfoPage.test.tsx
+++ src/pages/AdMoreInfoPage/AdMoreInfoPage.test.tsx
@@ -1,15 +1,23 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
 
 import AdInfoPage from "./AdMoreInfoPage";
 
 const mockUseParams = { id: "test" };
-
+const mockUseDispatch = jest.fn();
 jest.mock("react-router-dom", () => {
   return {
     ...jest.requireActual("react-router-dom"),
     useParams: () => mockUseParams,
+  };
+});
+
+jest.mock("react-redux", () => {
+  return {
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockUseDispatch,
   };
 });
 
@@ -27,6 +35,19 @@ describe("Given a AdInfoPage component", () => {
 
       expect(headings).toHaveLength(3);
       expect(button).toBeInTheDocument();
+    });
+  });
+  describe("When it is clicked on delete button", () => {
+    test("Then ii should call the dispatch function'", () => {
+      render(
+        <Provider store={store}>
+          <AdInfoPage />
+        </Provider>
+      );
+
+      const button = screen.getByRole("button", { name: /delete/i });
+      userEvent.click(button);
+      expect(mockUseDispatch).toHaveBeenCalled();
     });
   });
 });
