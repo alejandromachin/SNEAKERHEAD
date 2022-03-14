a8f249a7f905bb398e11758410eb587b45f8cafa
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-14 14:36:02 +0100

    added function to render the ads or not if the users buys o sells

---
+++ src/components/Button/ButtonStyles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SellButton = styled.div`
+  button {
+    cursor: pointer;
+    width: 100px;
+    height: 30px;
+    text-align: center;
+    font-size: 20px;
+    color: #ff0000;
+    border: none;
+    background-color: #d3e2ff;
+  }
+`;
