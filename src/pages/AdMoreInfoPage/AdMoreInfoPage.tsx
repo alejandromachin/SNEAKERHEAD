298c880413cbd86e514376db432650671ed41234
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-16 10:31:06 +0100

    added styles to editForm

--- src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
+++ src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
@@ -8,7 +8,12 @@ import { deleteAdThunk, moreInfoAdThunk } from "../../redux/thunks/adsThunks";
 import { Ad } from "../../Types/Ad";
 import { Sneaker } from "../../Types/Sneaker";
 import { ButtonContainer } from "../SneakerInfoPage/SneakerInfoPageStyles";
-import { AdMoreInfoContainer, AdMoreInfoText } from "./AdMoreInfoPageStyles";
+import {
+  AdMoreInfoContainer,
+  AdMoreInfoText,
+  DeleteButton,
+  FormContainer,
+} from "./AdMoreInfoPageStyles";
 
 const AdInfoPage = (): JSX.Element => {
   const { id } = useParams();
@@ -25,35 +30,48 @@ const AdInfoPage = (): JSX.Element => {
     dispatch(deleteAdThunk(id as string));
   };
 
+  const editForm = () => {
+    setShowEditForm(!showEditForm);
+  };
   return (
     <AdMoreInfoContainer>
       {(ad as Ad).images && (
         <img src={(ad as Ad).images[0]} alt={(ad as Ad).colorway}></img>
       )}
       <AdMoreInfoText>
-        <h1>
-          {(ad as Ad).brand} {(ad as Ad).style}
-        </h1>
-        <h2>{(ad as Ad).colorway} </h2>
-        <h3>{(ad as Ad).state}</h3>
-        <p>Price: {(ad as Ad).price}</p>
-        <p>Condition: {(ad as Ad).condition}/10</p>
-        <p>Box: {(ad as Ad).box}</p>
-        <p>Size: {(ad as Ad).size}</p>
-        {showEditForm && (
+        {showEditForm ? (
           <>
-            <AdForm
-              sneaker={sneaker as Sneaker}
-              text="EDIT"
-              userId={(ad as Ad).owner}
-              ad={ad as Ad}
-            />
+            <FormContainer>
+              <AdForm
+                sneaker={sneaker as Sneaker}
+                text="EDIT"
+                userId={(ad as Ad).owner}
+                ad={ad as Ad}
+              />
+              <DeleteButton>
+                <Button actionOnClick={deleteAd} text={"DELETE"} />
+              </DeleteButton>
+            </FormContainer>
+            <ButtonContainer>
+              <Button actionOnClick={editForm} text={"CANCEL"} />
+            </ButtonContainer>
+          </>
+        ) : (
+          <>
+            <h1>
+              {(ad as Ad).brand} {(ad as Ad).style}
+            </h1>
+            <h2>{(ad as Ad).colorway} </h2>
+            <h3>{(ad as Ad).state}</h3>
+            <p>Price: {(ad as Ad).price}</p>
+            <p>Condition: {(ad as Ad).condition}/10</p>
+            <p>Box: {(ad as Ad).box}</p>
+            <p>Size: {(ad as Ad).size}</p>
+            <ButtonContainer>
+              <Button actionOnClick={editForm} text={"EDIT"} />
+            </ButtonContainer>
           </>
         )}
-
-        <ButtonContainer>
-          <Button actionOnClick={deleteAd} text={"EDIT"} />
-        </ButtonContainer>
       </AdMoreInfoText>
     </AdMoreInfoContainer>
   );
