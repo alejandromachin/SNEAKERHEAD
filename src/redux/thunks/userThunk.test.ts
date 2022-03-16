3ebc75ce48359ecd3d0d35654d9127027663bb36
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-16 19:17:13 +0100

    added test for registerThunk

---
+++ src/redux/thunks/userThunk.test.ts
@@ -0,0 +1,19 @@
+import { registerThunk } from "./userThunk";
+
+describe("Given a registerThunk inner function", () => {
+  describe("When it is called with the data of an user", () => {
+    test("Then it should send a request to the url", async () => {
+      const userData = {
+        name: "",
+        lastname: "",
+        username: "",
+        password: "",
+        email: "",
+        city: "",
+        ads: [],
+      };
+
+      await registerThunk(userData);
+    });
+  });
+});
