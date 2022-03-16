032abfeffcb1ad12e198bea3e10ac7134151d561
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-16 09:43:32 +0100

    added conditional rendering to show adForm to edit

--- src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
+++ src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import AdForm from "../../components/AdForm/AdForm";
 import Button from "../../components/Button/Button";
 import { RootState } from "../../redux/store";
 import { deleteAdThunk, moreInfoAdThunk } from "../../redux/thunks/adsThunks";
 import { Ad } from "../../Types/Ad";
+import { Sneaker } from "../../Types/Sneaker";
 import { ButtonContainer } from "../SneakerInfoPage/SneakerInfoPageStyles";
 import { AdMoreInfoContainer, AdMoreInfoText } from "./AdMoreInfoPageStyles";
 
@@ -12,6 +14,8 @@ const AdInfoPage = (): JSX.Element => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const ad = useSelector((state: RootState) => state.ad);
+  const sneaker = useSelector((state: RootState) => state.sneaker);
+  const [showEditForm, setShowEditForm] = useState<boolean>(false);
 
   useEffect(() => {
     dispatch(moreInfoAdThunk(id as string));
@@ -36,6 +40,17 @@ const AdInfoPage = (): JSX.Element => {
         <p>Condition: {(ad as Ad).condition}/10</p>
         <p>Box: {(ad as Ad).box}</p>
         <p>Size: {(ad as Ad).size}</p>
+        {showEditForm && (
+          <>
+            <AdForm
+              sneaker={sneaker as Sneaker}
+              text="EDIT"
+              userId={(ad as Ad).owner}
+              ad={ad as Ad}
+            />
+          </>
+        )}
+
         <ButtonContainer>
           <Button actionOnClick={deleteAd} text={"EDIT"} />
         </ButtonContainer>
