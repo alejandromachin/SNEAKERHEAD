a060c4b744e00adaf683a4ab35f938932c478aff
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-16 19:34:18 +0100

    added form rendering with onsubmit

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,66 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AdFormContainer } from "../AdForm/AdFormStyles";
+
+interface LoginData {
+  username: string;
+  password: string;
+}
+
 const LoginForm = (): JSX.Element => {
-  return <></>;
+  const blankFields = {
+    name: "",
+    lastname: "",
+    username: "",
+    password: "",
+    email: "",
+    city: "",
+    ads: [],
+  };
+  const dispatch = useDispatch();
+
+  const [loginData, setLoginData] = useState<LoginData>(blankFields);
+
+  const resetForm = () => {
+    setLoginData(blankFields);
+  };
+
+  const registerUser = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(loginThunk(loginData));
+    resetForm();
+  };
+
+  const changeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <AdFormContainer>
+      <form onSubmit={registerUser} noValidate autoComplete="off">
+        <label htmlFor="username">Name: </label>
+        <input
+          type="text"
+          id="username"
+          onChange={changeData}
+          value={loginData.username}
+          required
+        />
+        <label htmlFor="password">Lastname: </label>
+        <input
+          type="password"
+          id="password"
+          onChange={changeData}
+          value={loginData.password}
+          required
+        />
+
+        <button type="submit">LOG IN</button>
+      </form>
+    </AdFormContainer>
+  );
 };
 export default LoginForm;
