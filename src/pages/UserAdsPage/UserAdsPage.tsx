9d69f8f7c0f33e8701c71176838611134f29d085
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-17 21:45:14 +0100

    added useEffect to dispatch thunk on userAdsPage

--- src/pages/UserAdsPage/UserAdsPage.tsx
+++ src/pages/UserAdsPage/UserAdsPage.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { userAdsThunk } from "../../redux/thunks/userThunk";
+import { User } from "../../Types/User";
+
 const UserAdsPage = (): JSX.Element => {
+  const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userAdsThunk((user as User).id));
+  });
+
   return <></>;
 };
 export default UserAdsPage;
