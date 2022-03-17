8e7eab00e5660ea6be4098a0be2febc91d2f3f91
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-17 13:02:08 +0100

    added style container

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,12 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+import { LoginFormContainer } from "./LoginPageStyles";
+
 const LoginPage = () => {
-  return <></>;
+  return (
+    <LoginFormContainer>
+      <h1>LOG IN:</h1>
+      <LoginForm />
+    </LoginFormContainer>
+  );
 };
 export default LoginPage;
