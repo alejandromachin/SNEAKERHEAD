237c3fbf33ea8a8b6242574657f7bc1b89440afe
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-17 12:14:25 +0100

    added test on rendering and action

---
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store";
+import LoginForm from "./LoginForm";
+
+const mockUseDispatch = jest.fn();
+
+jest.mock("react-redux", () => {
+  return {
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockUseDispatch,
+  };
+});
+
+describe("Given a LoginForm component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a input for the username and one for the password and a button with the text 'LOG IN'", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const username = screen.getByRole("textbox");
+      const password = screen.getByLabelText("Password:");
+      const button = screen.getByRole("button", { name: /log in/i });
+      userEvent.type(username, "test");
+
+      expect(username).toBeInTheDocument();
+      expect(password).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+    });
+  });
+  describe("When it is clicked on the login button", () => {
+    test("Then it should call the dispatch function", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const button = screen.getByRole("button", { name: /log in/i });
+      userEvent.click(button);
+
+      expect(mockUseDispatch).toHaveBeenCalled();
+    });
+  });
+});
