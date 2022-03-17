46086ae26990dceda497f5525bd17433b8ac7e14
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-17 13:49:31 +0100

    added container for RegisterPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,12 @@
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import { RegisterPageContainer } from "./RegisterPageStyles";
 
 const RegisterPage = (): JSX.Element => {
   return (
-    <>
+    <RegisterPageContainer>
       <h1>REGISTER:</h1>
       <RegisterForm />
-    </>
+    </RegisterPageContainer>
   );
 };
 
