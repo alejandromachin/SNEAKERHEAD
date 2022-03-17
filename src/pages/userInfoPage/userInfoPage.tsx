e5caa87a45f09aa950862faddb5fe270e1570d07
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-17 17:32:05 +0100

    added styles to the info and user navigation

--- src/pages/userInfoPage/userInfoPage.tsx
+++ src/pages/userInfoPage/userInfoPage.tsx
@@ -1,26 +1,38 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { RootState } from "../../redux/store";
+import {
+  UserInfo,
+  UserInfoContainer,
+  UserNav,
+  UserTittle,
+} from "./UserInfoPageStyles";
 
 const UserInfoPage = (): JSX.Element => {
   const user = useSelector((state: RootState) => state.user);
 
   return (
-    <>
-      <h1>Hello, {user.name}</h1>
-      <h2>Profile info:</h2>
-      <h3>Name:</h3>
-      <p>
-        {user.name} {user.lastname}
-      </p>
-      <h3>Username:</h3>
-      <p>{user.username}</p>
-      <h3>Email address:</h3>
-      <p>{user.email}</p>
-      <Link to="">Edit info</Link>
-      <Link to="">Log out</Link>
-      <Link to="">Items on sale</Link>
-    </>
+    <UserInfoContainer>
+      <UserTittle>
+        <h1>Hello, {user.name}</h1>
+      </UserTittle>
+      <UserInfo>
+        <h2>Profile info:</h2>
+        <h3>Name:</h3>
+        <p>
+          {user.name} {user.lastname}
+        </p>
+        <h3>Username:</h3>
+        <p>{user.username}</p>
+        <h3>Email address:</h3>
+        <p>{user.email}</p>
+      </UserInfo>
+      <UserNav>
+        <Link to="">Edit info</Link>
+        <Link to="">Log out</Link>
+        <Link to="">Items on sale</Link>
+      </UserNav>
+    </UserInfoContainer>
   );
 };
 
