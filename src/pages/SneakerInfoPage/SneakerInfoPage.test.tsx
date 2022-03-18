9940b99f80e6578e86c58c45b26dc5500c02ec0f
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-18 19:02:33 +0100

    added tests on sneakerinfopage

--- src/pages/SneakerInfoPage/SneakerInfoPage.test.tsx
+++ src/pages/SneakerInfoPage/SneakerInfoPage.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store";
+import { Ad } from "../../Types/Ad";
 import SneakerInfoPage from "./SneakerInfoPage";
 
 const mockUseParams = { id: "test" };
@@ -13,35 +15,131 @@ jest.mock("react-router-dom", () => {
   };
 });
 
+const mockUser = { name: "test", id: "test" };
+const mockAds: Ad[] = [
+  {
+    id: "test",
+    brand: "test",
+    style: "test",
+    colorway: "test",
+    condition: 10,
+    image1: "test1",
+    image2: "test2",
+    image3: "test3",
+    image4: "test",
+    price: "10.000",
+    size: 40,
+    likes: 0,
+    box: "good",
+    state: "new",
+    owner: "622b15710695a90af3e56a20",
+  },
+  {
+    id: "test",
+    brand: "test",
+    style: "test",
+    colorway: "test",
+    condition: 10,
+    image1: "test",
+    image2: "test",
+    image3: "test",
+    image4: "test",
+    price: "10.000",
+    size: 40,
+    likes: 0,
+    box: "good",
+    state: "new",
+    owner: "622b15710695a90af3e56a20",
+  },
+  {
+    id: "test",
+    brand: "test",
+    style: "test",
+    colorway: "test",
+    condition: 10,
+    image1: "test",
+    image2: "test",
+    image3: "test",
+    image4: "test",
+    price: "10.000",
+    size: 40,
+    likes: 0,
+    box: "good",
+    state: "new",
+    owner: "622b15710695a90af3e56a20",
+  },
+  {
+    id: "test",
+    brand: "test",
+    style: "test",
+    colorway: "test",
+    condition: 10,
+    image1: "test",
+    image2: "test",
+    image3: "test",
+    image4: "test",
+    price: "10.000",
+    size: 40,
+    likes: 0,
+    box: "good",
+    state: "new",
+    owner: "622b15710695a90af3e56a20",
+  },
+];
+const mockSneaker = {
+  id: "123",
+  brand: "Jordan",
+  style: "1",
+  colorway: "Chicago",
+  releaseDate: "april 1990",
+  image: "image",
+  averagePrice: "5.000€",
+};
+
+jest.mock("react-redux", () => {
+  return {
+    ...jest.requireActual("react-redux"),
+    useSelector: (cb: CallableFunction) =>
+      cb({ user: mockUser, ads: mockAds, sneaker: mockSneaker }),
+  };
+});
+
 describe("Given a SneakerInfoPage component", () => {
-  describe("When it is rendered", () => {
-    test("Then it should show an image and a button to sell and a button to load more", async () => {
+  describe("When it is rendered with a sneaker with 4 ads", () => {
+    test("Then it should show 1 image of the sneaker, 2 images of the ads and a button to sell and a button to load more", async () => {
       render(
-        <Provider store={store}>
-          <SneakerInfoPage />
-        </Provider>
+        <BrowserRouter>
+          <Provider store={store}>
+            <SneakerInfoPage />
+          </Provider>
+        </BrowserRouter>
       );
 
-      const image = screen.getByRole("img");
+      const images = screen.getAllByRole("img");
       const buttonSell = screen.getByRole("button", { name: "SELL" });
       const buttonLoad = screen.getByRole("button", { name: "LOAD MORE" });
 
-      expect(image).toBeInTheDocument();
+      expect(images).toHaveLength(3);
       expect(buttonSell).toBeInTheDocument();
       expect(buttonLoad).toBeInTheDocument();
     });
   });
   describe("When its button is clicked", () => {
-    test("Then it should load more 2 more ads", async () => {
+    test("Then it should load 2 more ads", async () => {
       render(
-        <Provider store={store}>
-          <SneakerInfoPage />
-        </Provider>
+        <BrowserRouter>
+          <Provider store={store}>
+            <SneakerInfoPage />
+          </Provider>
+        </BrowserRouter>
       );
 
       const buttonLoad = screen.getByRole("button", { name: "LOAD MORE" });
 
       userEvent.click(buttonLoad);
+
+      const images = screen.getAllByRole("img");
+      expect(images).toHaveLength(7);
     });
   });
 });
