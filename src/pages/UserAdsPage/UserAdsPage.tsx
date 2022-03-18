d942270b0d85ff45ae85417d136b899ae7229dd2
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-18 11:49:48 +0100

    added tittle and style imported from userInfoPage

--- src/pages/UserAdsPage/UserAdsPage.tsx
+++ src/pages/UserAdsPage/UserAdsPage.tsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { userAdsThunk } from "../../redux/thunks/userThunk";
 import { Ad } from "../../Types/Ad";
-import { UserAdsList } from "./UserAdsPageStyles";
+import { UserInfoContainer, UserTittle } from "../UserPage/UserInfoPageStyles";
+import { UserAdsInfo, UserAdsList, UserAdsPrice } from "./UserAdsPageStyles";
 
 const UserAdsPage = (): JSX.Element => {
   const user = useSelector((state: RootState) => state.user);
@@ -17,21 +18,30 @@ const UserAdsPage = (): JSX.Element => {
   }, [dispatch, id]);
 
   return (
-    <UserAdsList>
-      <ul>
-        {userAds.map((ad: Ad) => (
-          <li key={ad.id}>
-            <h2>
-              {ad.brand} {ad.style}
-            </h2>
-            <p>
-              {ad.colorway} | {ad.size}{" "}
-            </p>
-            <p>{ad.price}</p>
-          </li>
-        ))}
-      </ul>
-    </UserAdsList>
+    <UserInfoContainer>
+      <UserTittle>
+        <h1>Hello, {user.name}</h1>
+      </UserTittle>
+      <UserAdsList>
+        <ul>
+          {userAds.map((ad: Ad) => (
+            <li key={ad.id}>
+              <UserAdsInfo>
+                <h2>
+                  {ad.brand} {ad.style}
+                </h2>
+                <p>
+                  {ad.colorway} | size: {ad.size}{" "}
+                </p>
+              </UserAdsInfo>
+              <UserAdsPrice>
+                <p className="UserAds__price">{ad.price}</p>
+              </UserAdsPrice>
+            </li>
+          ))}
+        </ul>
+      </UserAdsList>
+    </UserInfoContainer>
   );
 };
 export default UserAdsPage;
--- src/pages/UserAdsPage/UserAdsPageStyles.tsx
+++ src/pages/UserAdsPage/UserAdsPageStyles.tsx
@@ -1,16 +1,45 @@
 import styled from "styled-components";
 
 export const UserAdsList = styled.div`
-  width: 100vw;
+  width: 100%;
+
   & ul {
+    margin-top: 50px;
     width: 100%;
     padding: 0px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    gap: 20px;
   }
   & li {
+    width: 80%;
+    height: 50px;
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
     list-style: none;
+    background-color: #d3e2ff;
+  }
+`;
+
+export const UserAdsInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  & h2,
+  p {
+    margin: 0px;
+  }
+`;
+export const UserAdsPrice = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & .UserAds__price {
+    font-size: 30px;
   }
 `;
