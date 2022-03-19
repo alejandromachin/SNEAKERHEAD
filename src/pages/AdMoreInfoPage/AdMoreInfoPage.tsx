9e8c45be63cd8b9e15c8d89a99c976240d34e77c
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-19 12:20:08 +0100

    added carousel for ad images

--- src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
+++ src/pages/AdMoreInfoPage/AdMoreInfoPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import Button from "../../components/Button/Button";
 import EditAdForm from "../../components/EditAdForm/EditAdForm";
 import { RootState } from "../../redux/store";
@@ -31,42 +33,56 @@ const AdMoreInfoPage = (): JSX.Element => {
   const editForm = () => {
     setShowEditForm(!showEditForm);
   };
+
   return (
-    <AdMoreInfoContainer>
-      {(ad as Ad).image1 && (
-        <img src={(ad as Ad).image1} alt={(ad as Ad).colorway}></img>
-      )}
-      <AdMoreInfoText>
-        {showEditForm ? (
-          <>
-            <FormContainer>
-              <EditAdForm ad={ad as Ad} actionOnEdit={editForm} />
-              <DeleteButton>
-                <Button actionOnClick={deleteAd} text={"DELETE"} />
-              </DeleteButton>
-            </FormContainer>
-            <ButtonContainer>
-              <Button actionOnClick={editForm} text={"CANCEL"} />
-            </ButtonContainer>
-          </>
-        ) : (
-          <>
-            <h1>
-              {(ad as Ad).brand} {(ad as Ad).style}
-            </h1>
-            <h2>{(ad as Ad).colorway} </h2>
-            <h3>{(ad as Ad).state}</h3>
-            <p>Price: {(ad as Ad).price}</p>
-            <p>Condition: {(ad as Ad).condition}/10</p>
-            <p>Box: {(ad as Ad).box}</p>
-            <p>Size: {(ad as Ad).size}</p>
-            <ButtonContainer>
-              <Button actionOnClick={editForm} text={"EDIT"} />
-            </ButtonContainer>
-          </>
-        )}
-      </AdMoreInfoText>
-    </AdMoreInfoContainer>
+    <>
+      <AdMoreInfoContainer>
+        <Carousel showThumbs={false} showStatus={false} showArrows={false}>
+          <div>
+            <img src={(ad as Ad).image1} alt={(ad as Ad).colorway}></img>
+          </div>
+          <div>
+            <img src={(ad as Ad).image1} alt={(ad as Ad).colorway}></img>
+          </div>
+          <div>
+            <img src={(ad as Ad).image1} alt={(ad as Ad).colorway}></img>
+          </div>
+          <div>
+            <img src={(ad as Ad).image4} alt={(ad as Ad).colorway}></img>
+          </div>
+        </Carousel>
+        <AdMoreInfoText>
+          {showEditForm ? (
+            <>
+              <FormContainer>
+                <EditAdForm ad={ad as Ad} actionOnEdit={editForm} />
+                <DeleteButton>
+                  <Button actionOnClick={deleteAd} text={"DELETE"} />
+                </DeleteButton>
+              </FormContainer>
+              <ButtonContainer>
+                <Button actionOnClick={editForm} text={"CANCEL"} />
+              </ButtonContainer>
+            </>
+          ) : (
+            <>
+              <h1>
+                {(ad as Ad).brand} {(ad as Ad).style}
+              </h1>
+              <h2>{(ad as Ad).colorway} </h2>
+              <h3>{(ad as Ad).state}</h3>
+              <p>Price: {(ad as Ad).price}</p>
+              <p>Condition: {(ad as Ad).condition}/10</p>
+              <p>Box: {(ad as Ad).box}</p>
+              <p>Size: {(ad as Ad).size}</p>
+              <ButtonContainer>
+                <Button actionOnClick={editForm} text={"EDIT"} />
+              </ButtonContainer>
+            </>
+          )}
+        </AdMoreInfoText>
+      </AdMoreInfoContainer>
+    </>
   );
 };
 export default AdMoreInfoPage;
