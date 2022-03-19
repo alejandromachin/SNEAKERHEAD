f0395bed42e684f89fd7f5fab95d7d13252527ca
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-19 12:43:32 +0100

    added images and slider in homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,24 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
 const HomePage = (): JSX.Element => {
   return (
     <>
       <h1>SNEAKERHEAD</h1>
+      <Carousel showThumbs={false} showStatus={false} showArrows={false}>
+        <div>
+          <img src="/images/homepage_slider/jordan1-bred.png" alt="bred"></img>
+        </div>
+        <div>
+          <img
+            src="/images/homepage_slider/mono-sunflower-1.png"
+            alt="sunflower"
+          ></img>
+        </div>
+        <div>
+          <img src="/images/homepage_slider/jordan4-Sail.png" alt="sail"></img>
+        </div>
+      </Carousel>
     </>
   );
 };
