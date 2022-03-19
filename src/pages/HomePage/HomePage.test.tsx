09b013a716308b6edc96fd084bb1257c4bb15d0c
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-19 13:18:06 +0100

    modified homePage test

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 import HomePage from "./HomePage";
 
 describe("Given a HomePage component", () => {
   describe("When it is instantiated", () => {
     test("Then it should show a heading with the text 'SNEAKERHEAD'", () => {
       const expectedHeading = "SNEAKERHEAD";
-      render(<HomePage />);
+      render(
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      );
 
       const heading = screen.getByRole("heading", { name: expectedHeading });
 
