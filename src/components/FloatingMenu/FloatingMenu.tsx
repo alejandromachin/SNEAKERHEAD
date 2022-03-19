1a20aca36f3d51a5b9c02da1f1f4d0fda089e7b8
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-19 20:09:31 +0100

    added styles to floating menu

--- src/components/FloatingMenu/FloatingMenu.tsx
+++ src/components/FloatingMenu/FloatingMenu.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 import { FloatingMenuDiv } from "./FloatingMenuStyles";
 
 interface FloatingMenuProps {
@@ -5,10 +7,21 @@ interface FloatingMenuProps {
 }
 
 const FloatingMenu = ({ isActive }: FloatingMenuProps): JSX.Element => {
+  const user = useSelector((state: RootState) => state.user);
+
   return (
     <>
       <FloatingMenuDiv className={isActive ? "active" : ""}>
-        <p>Hola</p>
+        <p className="FloatinMenu__greating">
+          Hello, {user.name ? user.name : "stranger"}
+        </p>
+        <ul>
+          <li className="FloatinMenu__Register">
+            {user.name ? "" : "Register"}
+          </li>
+          <li>Home</li>
+          <li>All sneakers</li>
+        </ul>
       </FloatingMenuDiv>
     </>
   );
--- src/components/FloatingMenu/FloatingMenuStyles.ts
+++ src/components/FloatingMenu/FloatingMenuStyles.ts
@@ -4,9 +4,11 @@ export const FloatingMenuDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
   height: 250px;
   width: 200px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  padding-left: 10px;
   top: 0px;
   left: -500px;
   position: absolute;
@@ -14,8 +16,34 @@ export const FloatingMenuDiv = styled.div`
   transition: all 0.25s;
   transition-timing-function: ease-in-out;
   z-index: 10;
+  box-shadow: 6px 6px 22px -11px rgba(0, 0, 0, 0.75);
   &.active {
     left: 0px;
+  }
+  & p {
+    width: 100px;
+    text-align: left;
+    font-size: 35px;
+    padding-bottom: 10px;
+    margin: 0px;
+    margin-top: 50px;
+    border-bottom: 1px solid #000;
+  }
+  & ul {
+    list-style: none;
+    margin-top: 0px;
+    padding: 0px;
+
+    & li {
+      font-size: 20px;
+      text-align: left;
+      margin: 5px;
+
+      &.FloatinMenu__Register {
+        color: #ff0000;
+        font-size: 25px;
+      }
+    }
   }
   @media (min-width: 600px) {
     display: none;
