c02a89d8f10d6e1943de4c76cf73c3abc0fc1ed6
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-19 14:42:47 +0100

    removed h1 tittle

--- src/components/HotDeals/HotDeals.tsx
+++ src/components/HotDeals/HotDeals.tsx
@@ -7,12 +7,12 @@ interface HotDealsProps {
 const HotDeals = ({ ads }: HotDealsProps): JSX.Element => {
   return (
     <>
-      <h1>HOT DEALS:</h1>
+      <h2>HOT DEALS:</h2>
       {ads.map((ad) => (
         <>
           <img src={ad.image1} alt={ad.colorway} />
-          <h2>{ad.brand} </h2>
-          <h3>{ad.colorway}</h3>
+          <h3>{ad.brand} </h3>
+          <p className="HotDeals__colorway">{ad.colorway}</p>
           <p className="HotDeals__price">{ad.price}</p>
         </>
       ))}
