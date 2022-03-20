b36a0137a75e70bc985365d68cd35bb2af183722
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-20 11:59:42 +0100

    added userReducer case for logoutAction and no action

--- src/redux/reducers/userReducer/userReducer.test.ts
+++ src/redux/reducers/userReducer/userReducer.test.ts
@@ -1,9 +1,18 @@
-import { LoginAction } from "../../../Types/Action";
+import { Action, LoginAction } from "../../../Types/Action";
 import { LoginData } from "../../../Types/LoginData";
 import actionTypes from "../../actions/actionTypes";
 import userReducer from "./userReducer";
 
 describe("Given a userReducer function", () => {
+  describe("When it is rendered with no action", () => {
+    test("Then it should return the same state as the current state", () => {
+      const currentState = {};
+
+      const newState = userReducer(currentState, undefined);
+
+      expect(newState).toEqual(currentState);
+    });
+  });
   describe("When it is called with an empty state and a loginAction with an user data", () => {
     test("Then it should return the user data as the new state", () => {
       const currentState = {};
@@ -20,6 +29,19 @@ describe("Given a userReducer function", () => {
       const newState = userReducer(currentState, action);
 
       expect(newState).toEqual(userData);
+    });
+  });
+  describe("When it is called  with a logOutAction", () => {
+    test("Then it should return an empty state as new state", () => {
+      const currentState = { user: "test" };
+      const expectedNewState = {};
+      const logOutAction: Action = {
+        type: actionTypes.logout,
+      };
+
+      const newState = userReducer(currentState, logOutAction);
+
+      expect(newState).toEqual(expectedNewState);
     });
   });
 });
