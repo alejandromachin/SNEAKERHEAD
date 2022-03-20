2e26f66489b132ebd1e9253bee418a0e389cc22d
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-20 14:43:08 +0100

    added test cases for ok and error

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -241,4 +241,31 @@ export const handlers = [
       ])
     );
   }),
+  rest.get(`${process.env.REACT_APP_URL}sneakers/spinner`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: "123",
+          brand: "Jordan",
+          style: "1 high",
+          colorway: "Chicago",
+          releaseDate: "1/2/1980",
+          image: "image",
+          averagePrice: "4.000€",
+          ads: [],
+        },
+        {
+          id: "123",
+          brand: "Jordan",
+          style: "1 high",
+          colorway: "Chicago",
+          releaseDate: "1/2/1980",
+          image: "image",
+          averagePrice: "4.000€",
+          ads: [],
+        },
+      ])
+    );
+  }),
 ];
--- src/redux/thunks/sneakersThunk.test.ts
+++ src/redux/thunks/sneakersThunk.test.ts
@@ -1,6 +1,11 @@
+import axios from "axios";
 import { LoadSneakersAction } from "../../Types/Action";
 import actionTypes from "../actions/actionTypes";
-import { loadAllSneakersThunk, moreInfoSneakerThunk } from "./sneakersThunk";
+import {
+  loadAllSneakersThunk,
+  loadSpinnerSneakersThunk,
+  moreInfoSneakerThunk,
+} from "./sneakersThunk";
 
 describe("Given a loadAllSneakersThunk function", () => {
   describe("When it's invoked", () => {
@@ -61,6 +66,57 @@ describe("Given a moreInfoSneakerThunk inner function", () => {
       const thunkFunction = moreInfoSneakerThunk(sneaker.id);
 
       await thunkFunction(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+describe("Given a loadSpinnerSneakersThunk function", () => {
+  describe("When it's invoked", () => {
+    test("Then it should call the dispatch function", async () => {
+      const sneakers = [
+        {
+          id: "123",
+          brand: "Jordan",
+          style: "1 high",
+          colorway: "Chicago",
+          releaseDate: "1/2/1980",
+          image: "image",
+          averagePrice: "4.000€",
+          ads: [],
+        },
+        {
+          id: "123",
+          brand: "Jordan",
+          style: "1 high",
+          colorway: "Chicago",
+          releaseDate: "1/2/1980",
+          image: "image",
+          averagePrice: "4.000€",
+          ads: [],
+        },
+      ];
+
+      const dispatch = jest.fn();
+      const action: LoadSneakersAction = {
+        type: actionTypes.loadSpinnerSneakers,
+        sneakers,
+      };
+
+      await loadSpinnerSneakersThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+  describe("When it has an error on fetching", () => {
+    test("Then it should call the dispatch function with the error action", async () => {
+      jest.mock("axios");
+
+      const error = { response: { data: "error" } };
+      axios.get = jest.fn().mockRejectedValue(error);
+      const dispatch = jest.fn();
+
+      await loadSpinnerSneakersThunk(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
