84cdd2c8799c3bd2a6ea4d07316d2570b83ce693
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-20 11:26:56 +0100

    added test on ErroOnLoginAction

--- src/redux/actions/errorsActionCreator/errorActionCreator.test.ts
+++ src/redux/actions/errorsActionCreator/errorActionCreator.test.ts
@@ -1,5 +1,5 @@
 import errorTypes from "../errorTypes";
-import { errorAction } from "./errorActionCreator";
+import { errorAction, errorOnLoginAction } from "./errorActionCreator";
 
 describe("Given an errorAction function", () => {
   describe("When it is called with an error message", () => {
@@ -15,6 +15,25 @@ describe("Given an errorAction function", () => {
       };
 
       const action = errorAction(error);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+describe("Given an errorOnLoginAction function", () => {
+  describe("When it is called with an error message", () => {
+    test("Then it should return an object with the type and the error message", () => {
+      const error = {
+        error: true,
+        message: "General error",
+        code: 404,
+      };
+      const expectedAction = {
+        type: errorTypes.login,
+        error,
+      };
+
+      const action = errorOnLoginAction(error);
 
       expect(action).toEqual(expectedAction);
     });
