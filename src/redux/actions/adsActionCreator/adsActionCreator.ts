bd36b3a3800e56224b22dd3d22ff9517c8f7a76a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-20 15:23:35 +0100

    modified actionCreator action on loadSneakerSpinners

--- src/redux/actions/adsActionCreator/adsActionCreator.ts
+++ src/redux/actions/adsActionCreator/adsActionCreator.ts
@@ -41,10 +41,6 @@ export const filterAdsBySizeAction = (ads: Ad[]): LoadAdsAction => ({
   type: actionTypes.filterAdsBySize,
   ads,
 });
-export const loadSpinnerAdsAction = (ads: Ad[]): LoadAdsAction => ({
-  type: actionTypes.loadSpinnerAds,
-  ads,
-});
 
 export const cleanUpAdsAction = (): Action => ({
   type: actionTypes.cleanUpAds,
