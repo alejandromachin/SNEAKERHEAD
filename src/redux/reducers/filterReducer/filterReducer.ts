583f419ca5d2cc8df1e16b241c5b9e1c1a17194f
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-22 20:42:04 +0100

    changed name of size action type and removed types on AdForm

--- src/redux/reducers/filterReducer/filterReducer.ts
+++ src/redux/reducers/filterReducer/filterReducer.ts
@@ -1,11 +1,11 @@
-import { Action, sizeFilterActionType } from "../../../Types/Action";
+import { Action, SizeFilterActionType } from "../../../Types/Action";
 import actionTypes from "../../actions/actionTypes";
 
 const filterReducer = (currentFilter: number[] = [], action: Action = {}) => {
   let newFilter: number[] | [];
   switch (action.type) {
     case actionTypes.filterBySize:
-      newFilter = [...currentFilter, (action as sizeFilterActionType).size];
+      newFilter = [...currentFilter, (action as SizeFilterActionType).size];
       break;
     case actionTypes.clearFilters:
       newFilter = [];
