fbf2d2711fa85abcf9a86fa3a0d55f0471a5181e
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-23 10:29:36 +0100

    removed duplicated lines in MSW mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,159 +1,64 @@
 import { rest } from "msw";
 
+const mockAd = {
+  id: "ad test",
+  brand: "test",
+  style: "test",
+  colorway: "test",
+  image1: "test",
+  image2: "test",
+  image3: "test",
+  image4: "test",
+  price: "test",
+  likes: 0,
+  size: 40,
+  condition: 10,
+  box: "good",
+  state: "new",
+  owner: "test",
+};
+const mockSneaker = {
+  id: "123",
+  brand: "Jordan",
+  style: "1 high",
+  colorway: "Chicago",
+  releaseDate: "1/2/1980",
+  image: "image",
+  averagePrice: "4.000€",
+  ads: [],
+};
+const mockUser = {
+  name: "test",
+  lastname: "test",
+  username: "test",
+  email: "test",
+  city: "test",
+  ads: ["test"],
+};
+
 export const handlers = [
   rest.get(`${process.env.REACT_APP_URL}sneakers/`, (req, res, ctx) => {
     req.url.searchParams.get("limit");
     req.url.searchParams.get("skip");
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "123",
-          brand: "Jordan",
-          style: "1 high",
-          colorway: "Chicago",
-          releaseDate: "1/2/1980",
-          image: "image",
-          averagePrice: "4.000€",
-          ads: [],
-        },
-        {
-          id: "123",
-          brand: "Jordan",
-          style: "1 high",
-          colorway: "Chicago",
-          releaseDate: "1/2/1980",
-          image: "image",
-          averagePrice: "4.000€",
-          ads: [],
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockSneaker, mockSneaker]));
   }),
   rest.get(`${process.env.REACT_APP_URL}ads/hotdeals/load`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test1",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          images: ["test"],
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-        },
-        {
-          id: "ad test2",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          images: ["test"],
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd, mockAd]));
   }),
   rest.get(`${process.env.REACT_APP_URL}sneakers/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "123",
-          brand: "Jordan",
-          style: "1 high",
-          colorway: "Chicago",
-          releaseDate: "1/2/1980",
-          image: "image",
-          averagePrice: "4.000€",
-          ads: [],
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockSneaker]));
   }),
   rest.get(`${process.env.REACT_APP_URL}ads/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          images: ["test"],
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.get(`${process.env.REACT_APP_URL}ads/detail/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          images: ["test"],
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.delete(`${process.env.REACT_APP_URL}ads/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          images: ["test"],
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.post(`${process.env.REACT_APP_URL}user/register`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          name: "test",
-          lastname: "test",
-          username: "test",
-          email: "test",
-          city: "test",
-          ads: ["test"],
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockUser]));
   }),
   rest.post(`${process.env.REACT_APP_URL}user/login`, (req, res, ctx) => {
     return res(
@@ -169,103 +74,16 @@ export const handlers = [
     );
   }),
   rest.post(`${process.env.REACT_APP_URL}ads/new`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          image1: "test",
-          image2: "test",
-          image3: "test",
-          image4: "test",
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-          owner: "test",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.patch(`${process.env.REACT_APP_URL}ads/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          image1: "test",
-          image2: "test",
-          image3: "test",
-          image4: "test",
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-          owner: "test",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.get(`${process.env.REACT_APP_URL}user/ads/test`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "ad test",
-          brand: "test",
-          style: "test",
-          colorway: "test",
-          image1: "test",
-          image2: "test",
-          image3: "test",
-          image4: "test",
-          price: "test",
-          likes: 0,
-          size: 40,
-          condition: 10,
-          box: "good",
-          state: "new",
-          owner: "test",
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockAd]));
   }),
   rest.get(`${process.env.REACT_APP_URL}sneakers/slider`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: "123",
-          brand: "Jordan",
-          style: "1 high",
-          colorway: "Chicago",
-          releaseDate: "1/2/1980",
-          image: "image",
-          averagePrice: "4.000€",
-          ads: [],
-        },
-        {
-          id: "123",
-          brand: "Jordan",
-          style: "1 high",
-          colorway: "Chicago",
-          releaseDate: "1/2/1980",
-          image: "image",
-          averagePrice: "4.000€",
-          ads: [],
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json([mockSneaker, mockSneaker]));
   }),
   rest.get(
     `${process.env.REACT_APP_URL}ads/test`,
@@ -273,31 +91,7 @@ export const handlers = [
     (req, res, ctx) => {
       req.url.searchParams.get("limit");
       req.url.searchParams.get("skip");
-      return res(
-        ctx.status(200),
-        ctx.json([
-          {
-            id: "123",
-            brand: "Jordan",
-            style: "1 high",
-            colorway: "Chicago",
-            releaseDate: "1/2/1980",
-            image: "image",
-            averagePrice: "4.000€",
-            ads: [],
-          },
-          {
-            id: "123",
-            brand: "Jordan",
-            style: "1 high",
-            colorway: "Chicago",
-            releaseDate: "1/2/1980",
-            image: "image",
-            averagePrice: "4.000€",
-            ads: [],
-          },
-        ])
-      );
+      return res(ctx.status(200), ctx.json([mockSneaker, mockSneaker]));
     }
   ),
 ];
